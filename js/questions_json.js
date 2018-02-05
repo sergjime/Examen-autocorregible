ce1cf8578a4a870e68baa8cfa8123af9fcf4d52c
sergjime <32309472+sergjime@users.noreply.github.com>
2018-02-06 00:34:10 +0100

    Add files via upload

--- js/questions_json.js
+++ js/questions_json.js
@@ -1,19 +1,20 @@
 // fichoro xml que está en el servidor rawgit
-var url="https://rawgit.com/sergjime/Examen-parte1/master/json/preguntas.json";
+var url="https://rawgit.com/sergjime/Examen-autocorregible/master/json/preguntas.json";
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
- if (this.readyState == 4 && this.status == 200) {
-  // función personalizada que gestiona la respuesta a la petición de fichero
-  gestionarJson(this.responseText); 
- }
+	if (this.readyState == 4 && this.status == 200) {
+		// función personalizada que gestiona la respuesta a la petición de fichero
+		gestionarJson(this.responseText); 
+	}
 };
 xhttp.open("GET", url, true); //url del fichero
 xhttp.send();
 
 // función personalizada que gestiona la respuesta a la petición de fichero
 function gestionarJson(dadesJson){
-  var object = JSON.parse(dadesJson);
-  document.getElementById("p1").innerHTML = object.questions.question[0].title;
-  document.getElementById("t1").innerHTML = object.questions.question[0].type;
-  document.getElementById("r1").innerHTML = object.questions.question[0].answer;
+	var object = JSON.parse(dadesJson);
+	document.getElementsByClassName("negcurs")[0].innerHTML = object.questions.question[0].title;
+	document.getElementsByClassName("negcurs")[0].innerHTML = object.questions.question[0].type;
+	document.getElementsByClassName("negcurs")[0].innerHTML = object.questions.question[0].answer;
+}
